c55a4cf336c427771b27e26a204363980d3d50a2
Damian Legawiec <damian@sparksolutions.co>
2020-04-18 22:37:56 +0200

    Retry JS feature tests

--- lib/spree_dev_tools/rspec/spec_helper.rb
+++ lib/spree_dev_tools/rspec/spec_helper.rb
@@ -35,4 +35,8 @@ RSpec.configure do |config|
   config.verbose_retry = true
   # show exception that triggers a retry if verbose_retry is set to true
   config.display_try_failure_messages = true
+
+  config.around :each, :js do |ex|
+    ex.run_with_retry retry: 3
+  end
 end
