9fc0c713a4a730bc0227b995afa0e0217193f401
Damian Legawiec <damian@getvendo.com>
2025-11-20 21:45:22 +0100

    Improve install generator

--- lib/spree_dev_tools/generators/install/install_generator.rb
+++ lib/spree_dev_tools/generators/install/install_generator.rb
@@ -7,6 +7,7 @@ module SpreeDevTools
     def self.source_paths
       paths = superclass.source_paths
       paths << File.expand_path('templates', __dir__)
+      paths << File.expand_path('../../rspec/support', __dir__)
       paths.flatten
     end
 
@@ -22,6 +23,27 @@ module SpreeDevTools
       return if File.exist?(Rails.root.join('config/initializers/spree_dev_tools.rb'))
 
       template 'spree_dev_tools.rb', 'config/initializers/spree_dev_tools.rb'
+    end
+
+    def inject_rspec_support_files
+      empty_directory 'spec/support'
+
+      template 'capybara.rb', 'spec/support/capybara.rb'
+      template 'action_text.rb', 'spec/support/action_text.rb'
+      template 'active_job.rb', 'spec/support/active_job.rb'
+      template 'database_cleaner.rb', 'spec/support/database_cleaner.rb'
+      template 'devise.rb', 'spec/support/devise.rb'
+      template 'factory_bot.rb', 'spec/support/factory_bot.rb'
+      template 'spree.rb', 'spec/support/spree.rb'
+    end
+
+    def enable_support_files_loading
+      rails_helper_path = Rails.root.join('spec/rails_helper.rb')
+      return unless File.exist?(rails_helper_path)
+
+      gsub_file 'spec/rails_helper.rb',
+                "# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }",
+                "Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }"
     end
   end
 end
