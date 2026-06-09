e2a320cd43d2fb86c1737eeb7791a32fd80e6482
Damian Legawiec <damian@getvendo.com>
2026-06-09 22:28:31 +0200

    Bump spree_dev_tools to 0.6.1

--- lib/spree_dev_tools/version.rb
+++ lib/spree_dev_tools/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module SpreeDevTools
-  VERSION = '0.6.0'
+  VERSION = '0.6.1'
 
   # Returns the version of the currently loaded SpreeDevTools as a
   # <tt>Gem::Version</tt>.
